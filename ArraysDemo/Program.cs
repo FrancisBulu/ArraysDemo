24de8cf28ab2db6589d88b07c1b2e3034d9c7aca
Francis Bulu <FrancisB@cursisten.intecbrussel.intra>
2020-10-14 10:46:40 +0200

    Add project files.

---
+++ ArraysDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 15;
+            int[] data = new int[3]; // array maken met enkel 3 plaats voor 3 verschilende integers
+        }
+    }
+}
